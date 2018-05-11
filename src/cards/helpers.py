1fdc2fda7f4f5cd5fe43f667995dc146e17518d7
wy <wuyu1998@sina.com>
2018-05-11 15:29:26 +0800

    取款

--- src/cards/helpers.py
+++ src/cards/helpers.py
@@ -2,12 +2,13 @@
 
 # python apps
 import datetime
+import time
 
 # django apps
 from django.db import transaction
 
 # our apps
-from .models import Card, CardHistory, CardOperateType
+from .models import Card, CardHistory, CardOperateType, CardStatus
 
 
 class NotAutoCommit:
@@ -40,11 +41,17 @@ def put_money(card, money):
     :return: None or Exception
     '''
     pass
+    print('put_money(): ...{}\n\t函数执行前，django自动事务: {}'.format(
+        datetime.datetime.now().isoformat(), transaction.get_autocommit()
+        ))
     s_status = '正常'
     s_operator_type = '存款'
 
     with NotAutoCommit():
+        print('django自动事务: {}'.format(transaction.get_autocommit()))
         try:
+            print('休眠30秒 ... {}'.format(datetime.datetime.now().isoformat()))
+            time.sleep(30)
             if card.status.name == s_status:
                 try:
                     operator_type = CardOperateType.objects.get(name=s_operator_type)
@@ -82,9 +89,10 @@ def put_money(card, money):
                 )
                 obj.save()
 
+                print('数据提交之前 ... {}'.format(datetime.datetime.now().isoformat()))
                 # 数据提交
                 transaction.commit()
-                print('数据提交!')
+                print('数据提交之后 ... {}'.format(datetime.datetime.now().isoformat()))
             else:
                 msg = '银行卡的状态错误. status: {}'.format(card.status.name)
                 raise ValueError(msg)
@@ -92,7 +100,9 @@ def put_money(card, money):
             # 数据回滚
             msg = '未知错误. e: {}'.format(e)
             transaction.rollback()
+            print('数据回滚 ... {}'.format(datetime.datetime.now().isoformat()))
             raise ValueError(msg)
+    print('\t函数返回前，django自动事务: {}'.format(transaction.get_autocommit()))
 
 
 
@@ -167,4 +177,112 @@ def put_money_2(card, money):
         # 恢复django自动事务的设置
         transaction.set_autocommit(old_autocommit)
         print('恢复django自动设置的设置')
+
+
+def get_money(card, money):
+    ''' 取款
+
+    :param card: 银行卡
+    :type card: Card
+    :param money: 发生金额
+    :type money: int
+    :return: None or Exception
+    '''
+    pass
+    s_status = '正常'
+    s_operator_type = '取款'
+
+    with NotAutoCommit():
+        try:
+            # 检查银行卡状态
+            check_CardStatus(card, s_status)
+
+            # 检查余额
+            if card.balance < money:
+                msg = '余额不足'
+                raise ValueError(msg)
+            elif card.balance_available < money:
+                msg = '可用余额不足'
+                raise ValueError(msg)
+
+            # 取银行卡的操作类型
+            operator_type = get_CardOperateType(s_operator_type)
+
+            # 业务发生前的数据
+            data_old = card.to_json()
+            # 取款
+            card.balance -= money
+            card.balance_available -= money
+            card.save()
+            # 业务发生后的数据
+            data_new = card.to_json()
+
+            # 写流水帐
+            remark = '''
+            时间：{now},
+            发生金额：{money},
+            业务发生前的数据：{data_old},
+            业务发生后的数据：{data_new},
+            '''.format(
+                now=datetime.datetime.now().isoformat(),
+                money=money,
+                data_old=data_old,
+                data_new=data_new,
+            )
+
+            # raise ValueError('调试')
+
+            obj = CardHistory(
+                card=card,
+                operator_type=operator_type,
+                remark=remark,
+            )
+            obj.save()
+
+            # 数据提交
+            transaction.commit()
+        except Exception as e:
+            # 数据回滚
+            msg = '未知错误. e: {}'.format(e)
+            transaction.rollback()
+            raise ValueError(msg)
+
+
+def check_CardStatus(card, status):
+    ''' 检查银行卡的状态
+
+    :param card: 银行卡
+    :type card: Card
+    :param status: 银行卡的状态
+    :type status: str or CartStatus
+    :return: None or ValueError
+    '''
+    if not isinstance(card, Card):
+        msg = '参数错误. card: {}'.format(card)
+        raise ValueError(msg)
+
+    if isinstance(status, CardStatus):
+        status = status.name
+    elif not isinstance(status, str):
+        msg = '参数错误. status: {}'.format(status)
+        raise ValueError(msg)
+
+    if card.status.name != status:
+        msg = '银行卡的状态错误. status: {}'.format(card.status.name)
+        raise ValueError(msg)
+
+
+def get_CardOperateType(s_operator_type):
+    ''' 取银行卡的操作类型
+
+    :param s_operator_type: 银行卡的操作类型
+    :type s_operator_type: str
+    :return: CardOperateType or ValueError
+    '''
+    try:
+        operator_type = CardOperateType.objects.get(name=s_operator_type)
+    except CardOperateType.DoesNotExist:
+        msg = '操作类型不存在. operator_type: {}'.format(s_operator_type)
+        raise ValueError(msg)
+    return operator_type
 
--- src/cards/tests.py
+++ src/cards/tests.py
@@ -1,3 +1,48 @@
+# -*- encoding: utf-8 -*-
+
+# django apps
 from django.test import TestCase
 
-# Create your tests here.
+# our apps
+from . import models, helpers
+
+
+class PutMoneyTestCase(TestCase):
+    ''' 存钱 '''
+
+    fixtures = ['initial_data.json', ]
+
+    def setUp(self):
+        ''' 初始化测试环境 '''
+        pass
+
+        c01 = models.Card(status_id=1)
+        c01.save()
+        print('c01.id: {}'.format(c01.id))
+        c02 = models.Card(status_id=1)
+        c02.save()
+        # self.arr_Card = [c01, c02]
+        # models.CardInfo.objects.bulk_create(self.arr_Card)
+
+        c01_info = models.CardInfo(name='张三', card_id=c01.id)
+        # c01_info.save()
+        c02_info = models.CardInfo(name='李四', card_id=c02.id)
+        # c02_info.save()
+        arr = [c01_info, c02_info]
+        models.CardInfo.objects.bulk_create(arr)
+        print('c01_info.id: {}'.format(c01_info.id))
+
+    def tearDown(self):
+        ''' 释放测试环境 '''
+        pass
+
+    def test_right(self):
+        ''' 正确的情况 '''
+        c01 = models.Card.objects.get(pk=1)
+        money = 100
+        helpers.put_money(c01, money)
+
+        c01 = models.Card.objects.get(pk=1)
+        self.assertEqual(c01.balance, money)
+
+
